dfdb5d1c56db0d14f2f360efea1aad9dee9973ea
Favourdev <favourapps17@gmail.com>
2022-06-21 04:05:23 +0000

    updated code

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -5,6 +5,5 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
     except:
-        return (False)
-    else:
-        return (True)
+        return False
+    return True
